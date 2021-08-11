8bd73b482e1168c859d8c379c9294a8fd8b425ae
Aleksandr Kiselev <a.kiselev@excelsior.one>
2021-08-11 12:49:24 +0300

    Fixed subuser API key creation request

--- pkg/model/subuser/createapikeyrequest.go
+++ pkg/model/subuser/createapikeyrequest.go
@@ -1,8 +1,9 @@
 package subuser
 
 type CreateAPIKeyRequest struct {
-	OtpToken string `json:"otpToken"`
-	SubUid int64 `json:"sub_uid"`
-	Note string `json:"note"`
+	OtpToken    string  `json:"otpToken"`
+	SubUid      int64   `json:"subUid"`
+	Note        string  `json:"note"`
+	Permission  string  `json:"permission"`
 	IpAddresses *string `json:"ipAddresses"`
 }
