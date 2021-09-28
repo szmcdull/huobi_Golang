e5cb5c958efb940267012fd328843d40163ca94f
Aleksandr Kiselev <a.kiselev@excelsior.one>
2021-09-28 17:38:18 +0300

    Added operator to order response

--- pkg/model/order/getorderresponse.go
+++ pkg/model/order/getorderresponse.go
@@ -5,19 +5,20 @@ type GetOrderResponse struct {
 	ErrorCode    string `json:"err-code"`
 	ErrorMessage string `json:"err-msg"`
 	Data         *struct {
-		AccountId        int     `json:"account-id"`
-		Amount           string  `json:"amount"`
-		Id               int64   `json:"id"`
-		ClientOrderId    string  `json:"client-order-id"`
-		Symbol           string  `json:"symbol"`
-		Price            string  `json:"price"`
-		StopPrice        *string `json:"stop-price,omitempty"`
-		CreatedAt        int64   `json:"created-at"`
-		Type             string  `json:"type"`
-		FilledAmount     string  `json:"field-amount"`
-		FilledCashAmount string  `json:"field-cash-amount"`
-		FilledFees       string  `json:"field-fees"`
-		Source           string  `json:"source"`
-		State            string  `json:"state"`
+		AccountId        int    `json:"account-id"`
+		Amount           string `json:"amount"`
+		Id               int64  `json:"id"`
+		ClientOrderId    string `json:"client-order-id"`
+		Symbol           string `json:"symbol"`
+		Price            string `json:"price"`
+		CreatedAt        int64  `json:"created-at"`
+		Type             string `json:"type"`
+		FilledAmount     string `json:"field-amount"`
+		FilledCashAmount string `json:"field-cash-amount"`
+		FilledFees       string `json:"field-fees"`
+		Source           string `json:"source"`
+		State            string `json:"state"`
+		StopPrice        string `json:"stop-price"`
+		Operator         string `json:"operator"`
 	}
 }
